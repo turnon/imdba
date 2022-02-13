4ce08dd5230f97cd028bb65332c4b446e30d74fb
ken <block24block@gmail.com>
2022-02-13 14:53:49 +0800

    test tags

--- tsv/tsv_test.go
+++ tsv/tsv_test.go
@@ -2,6 +2,7 @@ package tsv
 
 import (
 	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/turnon/imdba/util"
@@ -16,8 +17,14 @@ func TestIterateTitleBasic(t *testing.T) {
 
 	startYears := util.Set{}
 	endYears := util.Set{}
+
 	genres := util.Set{}
+	expectedGenres := util.NewSet(strings.Split("Action Adult Adventure Animation Biography Comedy Crime Documentary Drama Family Fantasy Film-Noir"+" "+
+		"Game-Show History Horror Music Musical Mystery News Reality-TV Romance Sci-Fi Short Sport Talk-Show Thriller War Western \\N", " ")...)
+
 	titleTypes := util.Set{}
+	expectedTitleTypes := util.NewSet(strings.Split("movie short tvEpisode tvMiniSeries tvMovie tvPilot tvSeries tvShort tvSpecial video videoGame", " ")...)
+
 	ids := util.Set{}
 	var count uint
 
@@ -39,9 +46,18 @@ func TestIterateTitleBasic(t *testing.T) {
 	t.Log("title type", titleTypes.SortedList())
 	t.Log("genres", genres.SortedList())
 
+	if !genres.IsSame(expectedGenres) {
+		t.Errorf("wrong genres, expected %d, actual: %d", len(expectedGenres), len(genres))
+	}
+
+	if !titleTypes.IsSame(expectedTitleTypes) {
+		t.Errorf("wrong genres, expected %d, actual: %d", len(expectedTitleTypes), len(titleTypes))
+	}
+
 	if idCount := uint(len(ids)); idCount != tsLinesCount {
 		t.Errorf("wrong id count, expected %d, actual: %d", tsLinesCount, idCount)
 	}
+
 	if count != tsLinesCount {
 		t.Errorf("wrong line count, expected %d, actual: %d", tsLinesCount, count)
 	}
@@ -56,8 +72,14 @@ func TestIterateNameBasic(t *testing.T) {
 
 	birthYears := util.Set{}
 	deathYears := util.Set{}
+
 	professions := util.Set{}
-	titles := util.Set{}
+	expectedProfessions := util.NewSet(strings.Split("actor actress animation_department art_department art_director assistant assistant_director camera_department"+" "+
+		"casting_department casting_director choreographer cinematographer composer costume_department costume_designer director editor editorial_department"+" "+
+		"electrical_department executive legal location_management make_up_department manager miscellaneous music_department producer production_department"+" "+
+		"production_designer production_manager publicist script_department set_decorator sound_department soundtrack special_effects stunts talent_agent"+" "+
+		"transportation_department visual_effects writer", " ")...)
+
 	ids := util.Set{}
 	var count uint
 
@@ -69,21 +91,22 @@ func TestIterateNameBasic(t *testing.T) {
 		for _, p := range r.PrimaryProfessionArray() {
 			professions.Add(p)
 		}
-		for _, t := range r.KnownForTitlesArray() {
-			titles.Add(t)
-		}
 
 		return nil
 	})
 
 	t.Log("birth year", birthYears.SortedList())
 	t.Log("death year", deathYears.SortedList())
-	t.Log("titles", len(titles))
-	t.Log("professions", professions.SortedList())
+
+	professions.Remove("")
+	if !professions.IsSame(expectedProfessions) {
+		t.Errorf("wrong professions, expected %d, actual: %d", len(expectedProfessions), len(professions))
+	}
 
 	if idCount := uint(len(ids)); idCount != tsLinesCount {
 		t.Errorf("wrong id count, expected %d, actual: %d", tsLinesCount, idCount)
 	}
+
 	if count != tsLinesCount {
 		t.Errorf("wrong line count, expected %d, actual: %d", tsLinesCount, count)
 	}
@@ -97,22 +120,18 @@ func TestIterateTitlePrincipal(t *testing.T) {
 	}
 
 	categories := util.Set{}
-	characters := util.Set{}
-	jobs := util.Set{}
+	expectedCategories := util.NewSet(strings.Split("actor actress archive_footage archive_sound cinematographer composer director editor producer production_designer self writer", " ")...)
 	var count uint
 
 	IterateTitlePrincipal(tsv, func(r *TitlePrincipalRow) error {
 		count += 1
 		categories.Add(r.Category)
-		jobs.Add(r.Job)
-		for _, c := range r.CharactersArray() {
-			characters.Add(c)
-		}
-
 		return nil
 	})
 
-	t.Log("categories", categories.SortedList())
+	if !categories.IsSame(expectedCategories) {
+		t.Errorf("wrong categories, expected %d, actual: %d", len(expectedCategories), len(categories))
+	}
 
 	if count != tsLinesCount {
 		t.Errorf("wrong line count, expected %d, actual: %d", tsLinesCount, count)
--- util/set.go
+++ util/set.go
@@ -4,11 +4,25 @@ import "sort"
 
 type Set map[string]struct{}
 
+var emptyStruct = struct{}{}
+
+func NewSet(strs ...string) Set {
+	s := Set{}
+	for _, str := range strs {
+		s.Add(str)
+	}
+	return s
+}
+
 func (s Set) Add(str string) {
 	if _, ok := s[str]; ok {
 		return
 	}
-	s[str] = struct{}{}
+	s[str] = emptyStruct
+}
+
+func (s Set) Remove(str string) {
+	delete(s, str)
 }
 
 func (s Set) List() []string {
@@ -23,4 +37,16 @@ func (s Set) SortedList() []string {
 	strs := s.List()
 	sort.Strings(strs)
 	return strs
+}
+
+func (s Set) IsSame(s2 Set) bool {
+	if len(s) != len(s2) {
+		return false
+	}
+	for key := range s {
+		if _, ok := s2[key]; !ok {
+			return false
+		}
+	}
+	return true
 }
